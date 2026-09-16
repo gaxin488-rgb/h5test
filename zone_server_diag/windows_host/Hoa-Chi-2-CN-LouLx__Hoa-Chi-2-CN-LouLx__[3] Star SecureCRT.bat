c51d528c6b3c92261cb5fcd58c3120772fa23132
gaxin488-rgb <gaxin488-rgb@users.noreply.github.com>
2026-09-16 08:41:24 +0700

    Add sanitized zone server diagnostics

---
+++ zone_server_diag/windows_host/Hoa-Chi-2-CN-LouLx__Hoa-Chi-2-CN-LouLx__[3] Star SecureCRT.bat
@@ -0,0 +1,24 @@
+
+@ECHO OFF
+@cls
+Color 02
+mode con:cols=70 lines=15
+
+cd ./LouLx-Game/SecureCRT
+start SecureCRTPortable.exe
+@cls
+Color 02
+@echo    //////////////////////////////////////////////////////////////
+@echo    /                      Start Complete!!!                     /
+@echo    /                                                            /
+@echo    /                    Share by LouLx Game                     /
+@echo    /                                                            /
+@echo    /        Channel: https://www.youtube.com/@loulx2023         /
+@echo    /                                                            /
+@echo    /            Blog: https://loukoala.blogspot.com             /
+@echo    /                                                            /	
+@echo    /              Donate Me: https://bit.ly/3siIZEb             /
+@echo    /                                                            /
+@echo    //////////////////////////////////////////////////////////////
+timeout /t 5
+exit
